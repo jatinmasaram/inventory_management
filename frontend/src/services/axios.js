1a0f01574170082a505fe11b84b4728497aeca3e
jatinmasaram <105914808+jatinmasaram@users.noreply.github.com>
2026-06-22 01:07:49 +0530

    configure production backend url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://127.0.0.1:8000",
+    "https://inventory-management-1-f2bh.onrender.com",
 });
 
 api.interceptors.request.use(
